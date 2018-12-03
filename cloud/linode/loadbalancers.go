854bdf515315db685fbb467cf9d329c1aac2a74c
Andrew Sauber <asauber@linode.com>
2018-12-03 16:18:34 -0500

    Remove stray debugging output

--- cloud/linode/loadbalancers.go
+++ cloud/linode/loadbalancers.go
@@ -184,7 +184,6 @@ func (l *loadbalancers) UpdateLoadBalancer(ctx context.Context, clusterName stri
 					return err
 				}
 
-				fmt.Printf("options: %+v\n", opt.GetUpdateOptions())
 				_, err = l.client.UpdateNodeBalancerConfig(ctx, lb.ID, nbc.ID, opt.GetUpdateOptions())
 				if err != nil {
 					return fmt.Errorf("Error updating NodeBalancer config: %v", err)
