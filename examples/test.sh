b9e7dfa09d3308ebf3026daea88a996ff9607bb2
phillc <15082+phillc@users.noreply.github.com>
2020-07-14 14:31:21 -0400

    Add https example

---
+++ examples/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+kubectl apply -f ./http-nginx.yaml
+kubectl apply -f ./tcp-nginx.yaml
+
+openssl req -newkey rsa:4096 \
+            -x509 \
+            -sha256 \
+            -days 3650 \
+            -nodes \
+            -out example.crt \
+            -keyout example.key \
+            -subj "/C=na/ST=na/L=na/O=na/OU=na/CN=na"
+
+kubectl delete secret example-secret || true
+kubectl create secret tls example-secret --cert=example.crt --key=example.key
+kubectl apply -f ./https-nginx.yaml
+
