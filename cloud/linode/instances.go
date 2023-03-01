23a8e2c1160c4228fdd6d07a3de819c60779b774
Ondrej Kokes <okokes@akamai.com>
2023-03-01 12:12:41 +0100

    removed v1 impl (apart from zones) and fixed tests

--- cloud/linode/instances.go
+++ cloud/linode/instances.go
@@ -17,14 +17,7 @@ type instances struct {
 	client Client
 }
 
-// TODO(PR): providing both APIs to keep the tests green and to enable
-// gradual migration
-type hybridInstances interface {
-	cloudprovider.Instances
-	cloudprovider.InstancesV2
-}
-
-func newInstances(client Client) hybridInstances {
+func newInstances(client Client) cloudprovider.InstancesV2 {
 	return &instances{client}
 }
 
@@ -34,50 +27,6 @@ type instanceNoIPAddressesError struct {
 
 func (e instanceNoIPAddressesError) Error() string {
 	return fmt.Sprintf("instance %d has no IP addresses", e.id)
-}
-
-func (i *instances) NodeAddresses(ctx context.Context, name types.NodeName) ([]v1.NodeAddress, error) {
-	ctx = sentry.SetHubOnContext(ctx)
-	sentry.SetTag(ctx, "node_name", string(name))
-
-	linode, err := linodeByName(ctx, i.client, name)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return nil, err
-	}
-
-	addresses, err := i.nodeAddresses(ctx, linode)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return nil, err
-	}
-
-	return addresses, nil
-}
-
-func (i *instances) NodeAddressesByProviderID(ctx context.Context, providerID string) ([]v1.NodeAddress, error) {
-	ctx = sentry.SetHubOnContext(ctx)
-	sentry.SetTag(ctx, "provider_id", providerID)
-
-	id, err := parseProviderID(providerID)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return nil, err
-	}
-
-	linode, err := linodeByID(ctx, i.client, id)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return nil, err
-	}
-
-	addresses, err := i.nodeAddresses(ctx, linode)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return nil, err
-	}
-
-	return addresses, nil
 }
 
 func (i *instances) nodeAddresses(ctx context.Context, linode *linodego.Instance) ([]v1.NodeAddress, error) {
@@ -104,110 +53,6 @@ func (i *instances) nodeAddresses(ctx context.Context, linode *linodego.Instance
 	// Allow Nodes to not have an InternalIP, if this proves problematic this will be reverted
 
 	return addresses, nil
-}
-
-func (i *instances) InstanceID(ctx context.Context, nodeName types.NodeName) (string, error) {
-	ctx = sentry.SetHubOnContext(ctx)
-	sentry.SetTag(ctx, "node_name", string(nodeName))
-
-	linode, err := linodeByName(ctx, i.client, nodeName)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return "", err
-	}
-	return strconv.Itoa(linode.ID), nil
-}
-
-func (i *instances) InstanceType(ctx context.Context, nodeName types.NodeName) (string, error) {
-	ctx = sentry.SetHubOnContext(ctx)
-	sentry.SetTag(ctx, "node_name", string(nodeName))
-
-	linode, err := linodeByName(ctx, i.client, nodeName)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return "", err
-	}
-	return linode.Type, nil
-}
-
-func (i *instances) InstanceTypeByProviderID(ctx context.Context, providerID string) (string, error) {
-	ctx = sentry.SetHubOnContext(ctx)
-	sentry.SetTag(ctx, "provider_id", providerID)
-
-	id, err := parseProviderID(providerID)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return "", err
-	}
-
-	sentry.SetTag(ctx, "linode_id", strconv.Itoa(id))
-
-	linode, err := linodeByID(ctx, i.client, id)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return "", err
-	}
-	return linode.Type, nil
-}
-
-func (i *instances) AddSSHKeyToAllInstances(_ context.Context, user string, keyData []byte) error {
-	return cloudprovider.NotImplemented
-}
-
-func (i *instances) CurrentNodeName(_ context.Context, hostname string) (types.NodeName, error) {
-	return types.NodeName(hostname), nil
-}
-
-func (i *instances) InstanceExistsByProviderID(ctx context.Context, providerID string) (bool, error) {
-	ctx = sentry.SetHubOnContext(ctx)
-	sentry.SetTag(ctx, "provider_id", providerID)
-
-	id, err := parseProviderID(providerID)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return false, err
-	}
-
-	sentry.SetTag(ctx, "linode_id", strconv.Itoa(id))
-
-	_, err = linodeByID(ctx, i.client, id)
-	if err != nil {
-		if apiError, ok := err.(*linodego.Error); ok && apiError.Code == http.StatusNotFound {
-			return false, nil
-		}
-		sentry.CaptureError(ctx, err)
-		return false, err
-	}
-
-	return true, nil
-}
-
-func (i *instances) InstanceShutdownByProviderID(ctx context.Context, providerID string) (bool, error) {
-	ctx = sentry.SetHubOnContext(ctx)
-	sentry.SetTag(ctx, "provider_id", providerID)
-
-	id, err := parseProviderID(providerID)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return false, err
-	}
-
-	sentry.SetTag(ctx, "linode_id", strconv.Itoa(id))
-
-	instance, err := linodeByID(ctx, i.client, id)
-	if err != nil {
-		sentry.CaptureError(ctx, err)
-		return false, err
-	}
-
-	// An instance is considered to be "shutdown" when it is
-	// in the process of shutting down, or already offline.
-	if instance.Status == linodego.InstanceOffline ||
-		instance.Status == linodego.InstanceShuttingDown {
-		return true, nil
-	}
-
-	return false, nil
 }
 
 func (i *instances) InstanceExists(ctx context.Context, node *v1.Node) (bool, error) {
@@ -267,18 +112,29 @@ func (i *instances) InstanceShutdown(ctx context.Context, node *v1.Node) (bool, 
 }
 
 func (i *instances) InstanceMetadata(ctx context.Context, node *v1.Node) (*cloudprovider.InstanceMetadata, error) {
-	providerID := node.Spec.ProviderID
+	var (
+		providerID = node.Spec.ProviderID
+		nodeName   = types.NodeName(node.Name)
+		linode     *linodego.Instance
+		err        error
+	)
 
-	// TODO(okokes): should we at all times rely on providerID or should we include
-	// lookups by node name as well?
-	id, err := parseProviderID(providerID)
-	if err != nil {
-		return nil, err
-	}
+	// TODO(okokes): abstract this away and reuse it in our other methods here
+	if providerID != "" {
+		id, err := parseProviderID(providerID)
+		if err != nil {
+			return nil, err
+		}
 
-	linode, err := linodeByID(ctx, i.client, id)
-	if err != nil {
-		return nil, err
+		linode, err = linodeByID(ctx, i.client, id)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		linode, err = linodeByName(ctx, i.client, nodeName)
+		if err != nil {
+			return nil, err
+		}
 	}
 
 	addresses, err := i.nodeAddresses(ctx, linode)
@@ -290,6 +146,7 @@ func (i *instances) InstanceMetadata(ctx context.Context, node *v1.Node) (*cloud
 	meta := &cloudprovider.InstanceMetadata{
 		ProviderID:    providerID,
 		NodeAddresses: addresses,
+		InstanceType:  linode.Type,
 		Region:        linode.Region,
 	}
 
--- cloud/linode/instances_test.go
+++ cloud/linode/instances_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 	v1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/types"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	cloudprovider "k8s.io/cloud-provider"
 )
 
@@ -19,6 +19,10 @@ func nodeWithProviderID(providerID string) *v1.Node {
 	return &v1.Node{Spec: v1.NodeSpec{
 		ProviderID: providerID,
 	}}
+}
+
+func nodeWithName(name string) *v1.Node {
+	return &v1.Node{ObjectMeta: metav1.ObjectMeta{Name: name}}
 }
 
 func TestInstanceExistsByProviderID(t *testing.T) {
@@ -75,17 +79,19 @@ func TestNodeAddresses(t *testing.T) {
 
 	t.Run("errors when linode does not exist", func(t *testing.T) {
 		name := "does-not-exist"
+		node := nodeWithName(name)
 		filter := linodeFilterListOptions(name)
 		client.EXPECT().ListInstances(gomock.Any(), filter).Times(1).Return([]linodego.Instance{}, nil)
 
-		addrs, err := instances.NodeAddresses(ctx, types.NodeName(name))
+		meta, err := instances.InstanceMetadata(ctx, node)
 		assert.ErrorIs(t, err, cloudprovider.InstanceNotFound)
-		assert.Nil(t, addrs)
+		assert.Nil(t, meta)
 	})
 
 	t.Run("errors when linode does not have any ips", func(t *testing.T) {
 		id := 29392
 		name := "an-instance"
+		node := nodeWithName(name)
 		filter := linodeFilterListOptions(name)
 		client.EXPECT().ListInstances(gomock.Any(), filter).Times(1).Return([]linodego.Instance{
 			{ID: id, Label: name},
@@ -97,14 +103,15 @@ func TestNodeAddresses(t *testing.T) {
 			},
 		}, nil)
 
-		addrs, err := instances.NodeAddresses(ctx, types.NodeName(name))
+		meta, err := instances.InstanceMetadata(ctx, node)
 		assert.Error(t, err, instanceNoIPAddressesError{id})
-		assert.Nil(t, addrs)
+		assert.Nil(t, meta)
 	})
 
 	t.Run("should return addresses when linode is found", func(t *testing.T) {
 		id := 123
 		name := "mock-instance"
+		node := nodeWithName(name)
 		publicIPv4 := "45.76.101.25"
 		privateIPv4 := "192.168.133.65"
 		filter := linodeFilterListOptions(name)
@@ -118,9 +125,9 @@ func TestNodeAddresses(t *testing.T) {
 			},
 		}, nil)
 
-		addrs, err := instances.NodeAddresses(ctx, types.NodeName(name))
+		meta, err := instances.InstanceMetadata(ctx, node)
 		assert.NoError(t, err)
-		assert.Equal(t, addrs, []v1.NodeAddress{
+		assert.Equal(t, meta.NodeAddresses, []v1.NodeAddress{
 			{
 				Type:    v1.NodeHostName,
 				Address: name,
@@ -147,35 +154,38 @@ func TestNodeAddressesByProviderID(t *testing.T) {
 
 	t.Run("fails on malformed providerID", func(t *testing.T) {
 		providerID := "bogus://bogus"
-		addrs, err := instances.NodeAddressesByProviderID(ctx, providerID)
-
+		node := nodeWithProviderID(providerID)
+		meta, err := instances.InstanceMetadata(ctx, node)
 		assert.ErrorIs(t, err, invalidProviderIDError{providerID})
-		assert.Nil(t, addrs)
+		assert.Nil(t, meta)
 	})
 
 	t.Run("fails on non-numeric providerID", func(t *testing.T) {
 		providerID := providerIDPrefix + "abc"
-		addrs, err := instances.NodeAddressesByProviderID(ctx, providerID)
+		node := nodeWithProviderID(providerID)
+		meta, err := instances.InstanceMetadata(ctx, node)
 
 		assert.ErrorIs(t, err, invalidProviderIDError{providerID})
-		assert.Nil(t, addrs)
+		assert.Nil(t, meta)
 	})
 
 	t.Run("fails when linode does not exist", func(t *testing.T) {
 		id := 456302
 		providerID := providerIDPrefix + strconv.Itoa(id)
+		node := nodeWithProviderID(providerID)
 		getInstanceErr := &linodego.Error{Code: http.StatusNotFound}
 		client.EXPECT().GetInstance(gomock.Any(), id).Times(1).Return(nil, getInstanceErr)
-		addrs, err := instances.NodeAddressesByProviderID(ctx, providerID)
+		meta, err := instances.InstanceMetadata(ctx, node)
 
 		assert.ErrorIs(t, err, getInstanceErr)
-		assert.Nil(t, addrs)
+		assert.Nil(t, meta)
 	})
 
 	t.Run("should return addresses when linode is found", func(t *testing.T) {
 		id := 192910
 		name := "my-instance"
 		providerID := providerIDPrefix + strconv.Itoa(id)
+		node := nodeWithProviderID(providerID)
 		publicIPv4 := "32.74.121.25"
 		privateIPv4 := "192.168.121.42"
 		client.EXPECT().GetInstance(gomock.Any(), id).Times(1).Return(&linodego.Instance{
@@ -188,10 +198,10 @@ func TestNodeAddressesByProviderID(t *testing.T) {
 			},
 		}, nil)
 
-		addrs, err := instances.NodeAddressesByProviderID(ctx, providerID)
+		meta, err := instances.InstanceMetadata(ctx, node)
 
 		assert.NoError(t, err)
-		assert.Equal(t, addrs, []v1.NodeAddress{
+		assert.Equal(t, meta.NodeAddresses, []v1.NodeAddress{
 			{
 				Type:    v1.NodeHostName,
 				Address: name,
@@ -208,38 +218,6 @@ func TestNodeAddressesByProviderID(t *testing.T) {
 	})
 }
 
-func TestInstanceID(t *testing.T) {
-	ctx := context.TODO()
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	client := NewMockClient(ctrl)
-	instances := newInstances(client)
-
-	t.Run("fails when instance not found", func(t *testing.T) {
-		name := "non-existant-instance"
-		filter := linodeFilterListOptions(name)
-		client.EXPECT().ListInstances(gomock.Any(), filter).Times(1).Return([]linodego.Instance{}, nil)
-		instanceID, err := instances.InstanceID(ctx, types.NodeName(name))
-
-		assert.ErrorIs(t, err, cloudprovider.InstanceNotFound)
-		assert.Zero(t, instanceID)
-	})
-
-	t.Run("succeeds when instance exists", func(t *testing.T) {
-		id := 129289
-		name := "existing-instance"
-		filter := linodeFilterListOptions(name)
-		client.EXPECT().ListInstances(gomock.Any(), filter).Times(1).Return([]linodego.Instance{
-			{ID: id, Label: name},
-		}, nil)
-		instanceID, err := instances.InstanceID(ctx, types.NodeName(name))
-
-		assert.NoError(t, err)
-		assert.Equal(t, strconv.Itoa(id), instanceID)
-	})
-}
-
 func TestInstanceType(t *testing.T) {
 	ctx := context.TODO()
 	ctrl := gomock.NewController(t)
@@ -250,26 +228,32 @@ func TestInstanceType(t *testing.T) {
 
 	t.Run("fails when instance not found", func(t *testing.T) {
 		name := "bogus"
+		node := nodeWithName(name)
 		filter := linodeFilterListOptions(name)
 		client.EXPECT().ListInstances(gomock.Any(), filter).Times(1).Return([]linodego.Instance{}, nil)
-		instanceType, err := instances.InstanceType(ctx, types.NodeName(name))
+		meta, err := instances.InstanceMetadata(ctx, node)
 
 		assert.ErrorIs(t, err, cloudprovider.InstanceNotFound)
-		assert.Empty(t, instanceType)
+		assert.Nil(t, meta)
 	})
 
 	t.Run("succeeds when instance exists", func(t *testing.T) {
 		id := 39399
 		name := "my-nanode"
+		node := nodeWithName(name)
 		linodeType := "g6-standard-1"
 		filter := linodeFilterListOptions(name)
 		client.EXPECT().ListInstances(gomock.Any(), filter).Times(1).Return([]linodego.Instance{
 			{ID: id, Label: name, Type: linodeType},
 		}, nil)
-		instanceType, err := instances.InstanceType(ctx, types.NodeName(name))
+		client.EXPECT().GetInstanceIPAddresses(gomock.Any(), id).Times(1).Return(&linodego.InstanceIPAddressResponse{
+			IPv4: &linodego.InstanceIPv4Response{Public: []*linodego.InstanceIP{{Address: "1.2.3.4"}}},
+			IPv6: &linodego.InstanceIPv6Response{},
+		}, nil)
+		meta, err := instances.InstanceMetadata(ctx, node)
 
 		assert.NoError(t, err)
-		assert.Equal(t, linodeType, instanceType)
+		assert.Equal(t, linodeType, meta.InstanceType)
 	})
 }
 
@@ -284,39 +268,31 @@ func TestInstanceTypeByProviderID(t *testing.T) {
 	t.Run("fails when instance not found", func(t *testing.T) {
 		id := 39281
 		providerID := providerIDPrefix + strconv.Itoa(id)
+		node := nodeWithProviderID(providerID)
 		client.EXPECT().GetInstance(gomock.Any(), id).Times(1).Return(nil, linodego.Error{Code: http.StatusNotFound})
-		instanceType, err := instances.InstanceTypeByProviderID(ctx, providerID)
+		meta, err := instances.InstanceMetadata(ctx, node)
 
 		assert.Error(t, err)
-		assert.Empty(t, instanceType)
+		assert.Nil(t, meta)
 	})
 
 	t.Run("succeeds when instance exists", func(t *testing.T) {
 		id := 39281
 		providerID := providerIDPrefix + strconv.Itoa(id)
+		node := nodeWithProviderID(providerID)
 		linodeType := "g6-standard-2"
 		client.EXPECT().GetInstance(gomock.Any(), id).Times(1).Return(&linodego.Instance{
 			ID: id, Label: "some-linode", Type: linodeType,
 		}, nil)
-		instanceType, err := instances.InstanceTypeByProviderID(ctx, providerID)
+		client.EXPECT().GetInstanceIPAddresses(gomock.Any(), id).Times(1).Return(&linodego.InstanceIPAddressResponse{
+			IPv4: &linodego.InstanceIPv4Response{Public: []*linodego.InstanceIP{{Address: "1.2.3.4"}}},
+			IPv6: &linodego.InstanceIPv6Response{},
+		}, nil)
+		meta, err := instances.InstanceMetadata(ctx, node)
 
 		assert.NoError(t, err)
-		assert.Equal(t, linodeType, instanceType)
+		assert.Equal(t, linodeType, meta.InstanceType)
 	})
-}
-
-func TestCurrentNodeName(t *testing.T) {
-	instances := newInstances(nil)
-	hostname := "lke2901-2920fa-3838f-28a21"
-	nodeName, err := instances.CurrentNodeName(context.TODO(), hostname)
-	assert.NoError(t, err)
-	assert.Equal(t, types.NodeName(hostname), nodeName)
-}
-
-func TestAddSSHKeyToAllInstances(t *testing.T) {
-	instances := newInstances(nil)
-	err := instances.AddSSHKeyToAllInstances(context.TODO(), "root", []byte{})
-	assert.ErrorIs(t, err, cloudprovider.NotImplemented)
 }
 
 func TestInstanceShutdownByProviderID(t *testing.T) {
