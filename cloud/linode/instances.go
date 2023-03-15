556fff742701cce5284f693f631170297a886913
Ondrej Kokes <okokes@akamai.com>
2023-03-15 10:16:50 +0100

    Reintroduce Sentry handling

--- cloud/linode/instances.go
+++ cloud/linode/instances.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/linode/linode-cloud-controller-manager/sentry"
 	"github.com/linode/linodego"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
@@ -31,11 +33,16 @@ func (i *instances) lookupLinode(ctx context.Context, node *v1.Node) (*linodego.
 	providerID := node.Spec.ProviderID
 	nodeName := types.NodeName(node.Name)
 
+	sentry.SetTag(ctx, "provider_id", providerID)
+	sentry.SetTag(ctx, "node_name", node.Name)
+
 	if providerID != "" {
 		id, err := parseProviderID(providerID)
 		if err != nil {
+			sentry.CaptureError(ctx, err)
 			return nil, err
 		}
+		sentry.SetTag(ctx, "linode_id", strconv.Itoa(id))
 
 		return linodeByID(ctx, i.client, id)
 	}
@@ -44,10 +51,12 @@ func (i *instances) lookupLinode(ctx context.Context, node *v1.Node) (*linodego.
 }
 
 func (i *instances) InstanceExists(ctx context.Context, node *v1.Node) (bool, error) {
+	ctx = sentry.SetHubOnContext(ctx)
 	if _, err := i.lookupLinode(ctx, node); err != nil {
 		if apiError, ok := err.(*linodego.Error); ok && apiError.Code == http.StatusNotFound {
 			return false, nil
 		}
+		sentry.CaptureError(ctx, err)
 		return false, err
 	}
 
@@ -55,8 +64,10 @@ func (i *instances) InstanceExists(ctx context.Context, node *v1.Node) (bool, er
 }
 
 func (i *instances) InstanceShutdown(ctx context.Context, node *v1.Node) (bool, error) {
+	ctx = sentry.SetHubOnContext(ctx)
 	instance, err := i.lookupLinode(ctx, node)
 	if err != nil {
+		sentry.CaptureError(ctx, err)
 		return false, err
 	}
 
@@ -71,13 +82,17 @@ func (i *instances) InstanceShutdown(ctx context.Context, node *v1.Node) (bool, 
 }
 
 func (i *instances) InstanceMetadata(ctx context.Context, node *v1.Node) (*cloudprovider.InstanceMetadata, error) {
+	ctx = sentry.SetHubOnContext(ctx)
 	linode, err := i.lookupLinode(ctx, node)
 	if err != nil {
+		sentry.CaptureError(ctx, err)
 		return nil, err
 	}
 
 	if len(linode.IPv4) == 0 {
-		return nil, instanceNoIPAddressesError{linode.ID}
+		err := instanceNoIPAddressesError{linode.ID}
+		sentry.CaptureError(ctx, err)
+		return nil, err
 	}
 
 	addresses := []v1.NodeAddress{{Type: v1.NodeHostName, Address: linode.Label}}
