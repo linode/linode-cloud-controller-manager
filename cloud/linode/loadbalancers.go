c8ede4eddf5ef527f3c6545f942843d412eaaf54
Rahul Sharma <rahsharm@akamai.com>
2025-01-28 05:10:30 +0000

    delete fw if provisioned by CCM with ACL

--- cloud/linode/loadbalancers.go
+++ cloud/linode/loadbalancers.go
@@ -558,6 +558,12 @@ func (l *loadbalancers) EnsureLoadBalancerDeleted(ctx context.Context, clusterNa
 		return nil
 	}
 
+	fwClient := firewall.LinodeClient{Client: l.client}
+	err = fwClient.DeleteNodeBalancerFirewall(ctx, service, nb)
+	if err != nil {
+		return err
+	}
+
 	if err = l.client.DeleteNodeBalancer(ctx, nb.ID); err != nil {
 		klog.Errorf("failed to delete NodeBalancer (%d) for service (%s): %s", nb.ID, serviceNn, err)
 		sentry.CaptureError(ctx, err)
