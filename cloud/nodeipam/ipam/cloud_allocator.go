f56e373316d07e6a184b6d12a86bfaab6773f023
Khaja Omer <komer@akamai.com>
2026-05-08 12:52:01 -0500

    add clarification comment

--- cloud/nodeipam/ipam/cloud_allocator.go
+++ cloud/nodeipam/ipam/cloud_allocator.go
@@ -103,6 +103,9 @@ func NewLinodeCIDRAllocator(ctx context.Context, linodeClient linode.Client, cli
 		return nil, err
 	}
 
+	// Using Linode API, check if we need to reserve the final block in the cluster CIDR.
+	// Reserve when cluster CIDR last IP is the same as the VPC subnet last IP. 
+	// We cannot reserve that block since the last IP is a reserved IP for VPC functionality.
 	reserveFinalIPv4Block, err := shouldReserveFinalIPv4Block(ctx, linodeClient, allocatorParams.ClusterCIDRs[0])
 	if err != nil {
 		return nil, err
